176707842982f757f74362f1e0d13d86957fffc0
jweir136 <jweir136@yahoo.com>
2022-02-21 15:11:42 -0500

    add code that draws single blue circle on white background

---
+++ PyGame-1/main.py
@@ -0,0 +1,19 @@
+# import the module and init pygame
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([500, 500])
+running = True
+
+if __name__ == "__main__":
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+
+		screen.fill((255, 255, 255))
+		pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+		pygame.display.flip()
+
+	
+	pygame.quit()
